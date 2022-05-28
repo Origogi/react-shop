30017096aa2e9e73d7e219ca9e59ab2ee7ed4f5d
Origogi <rlawjdxo1212@naver.com>
2022-05-28 21:25:38 +0900

    샘플 상품 3개 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import "./App.css";
-import { Button, Nav, Navbar, Container } from "react-bootstrap";
+import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
+import bg from "./img/main-bg.jpg";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div>
+      <Container>
+        <Row>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+          <Col>
+            {" "}
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+          <Col>
+            {" "}
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품 설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
