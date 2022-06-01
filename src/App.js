50bc69ef56a46f91ba23274b33b05c2018938fab
Origogi <rlawjdxo1212@naver.com>
2022-06-01 22:15:00 +0900

    ShoesComp 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
-import { Button, Nav, Navbar, Container, Row, Col } from "react-bootstrap";
+import { Nav, Navbar, Container, Row, Col } from "react-bootstrap";
 import bg from "./img/bg.png";
+import { useState } from "react";
+import data from "./data.js";
 
 function App() {
+  let [shoes] = useState(data);
+
+  console.log(shoes);
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -14,39 +20,45 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-
       <div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div>
+
       <Container>
         <Row>
           <Col>
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width="80%"
-            ></img>
-            <h4>상품명</h4>
-            <p>상품 설명</p>
+            <ShoesComp
+              title={shoes[0].title}
+              price={shoes[0].price}
+              imgSrc="https://codingapple1.github.io/shop/shoes1.jpg"
+            />
           </Col>
           <Col>
-            {" "}
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width="80%"
-            ></img>
-            <h4>상품명</h4>
-            <p>상품 설명</p>
+            <ShoesComp
+              title={shoes[1].title}
+              price={shoes[1].price}
+              imgSrc="https://codingapple1.github.io/shop/shoes2.jpg"
+            />
           </Col>
           <Col>
-            {" "}
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width="80%"
-            ></img>
-            <h4>상품명</h4>
-            <p>상품 설명</p>
+            <ShoesComp
+              title={shoes[2].title}
+              price={shoes[2].price}
+              imgSrc="https://codingapple1.github.io/shop/shoes3.jpg"
+            />
           </Col>
         </Row>
       </Container>
     </div>
+  );
+}
+
+function ShoesComp(props) {
+  return (
+    <Col>
+      {" "}
+      <img src={props.imgSrc} width="80%"></img>
+      <h4>{props.title}</h4>
+      <p>{props.price}</p>
+    </Col>
   );
 }
 
