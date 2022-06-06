b8a92145b39938da2090d3714871dc037158a05d
Origogi <rlawjdxo1212@naver.com>
2022-06-06 16:20:59 +0900

    fade inout 애니메이션 추가

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -7,6 +7,7 @@ function Detail(props) {
 
   let [item] = useState(props.shoes.find((item) => item.id === Number(id)));
   let [tabIndex, setTabIndex] = useState(0);
+  let [fade, setFade] = useState("");
 
   useEffect(() => {
     console.log("컴포넌트가 화면에 나타남");
@@ -15,86 +16,84 @@ function Detail(props) {
     };
   }, []);
 
-  const [value, setValue] = useState("");
-
   useEffect(() => {
-    console.log(value);
-  }, [value]);
-
-  console.log(tabIndex);
+    setFade("fade_end");
+  });
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            src={`https://codingapple1.github.io/shop/shoes${
-              Number(id) + 1
-            }.jpg`}
-            width="100%"
-          />
+    <div className={`fade_start ${fade}`}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img
+              src={`https://codingapple1.github.io/shop/shoes${
+                Number(id) + 1
+              }.jpg`}
+              width="100%"
+            />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{item.title}</h4>
+            <p>{item.content}</p>
+            <p>{item.price}원</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
         </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">{item.title}</h4>
-          <p>{item.content}</p>
-          <p>{item.price}원</p>
-          <button className="btn btn-danger">주문하기</button>
-        </div>
-      </div>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-      <Nav variant="tabs" defaultActiveKey="link0">
-        <Nav.Item>
-          <Nav.Link
-            eventKey="link0"
-            onClick={() => {
-              setTabIndex(0);
-            }}
-          >
-            버튼0
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="link1"
-            onClick={() => {
-              setTabIndex(1);
-            }}
-          >
-            버튼1
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="link2"
-            onClick={() => {
-              setTabIndex(2);
-            }}
-          >
-            버튼2
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
 
-      <TabContent index={tabIndex} />
+        <Nav variant="tabs" defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link0"
+              onClick={() => {
+                setTabIndex(0);
+              }}
+            >
+              버튼0
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link1"
+              onClick={() => {
+                setTabIndex(1);
+              }}
+            >
+              버튼1
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link2"
+              onClick={() => {
+                setTabIndex(2);
+              }}
+            >
+              버튼2
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <TabContent index={tabIndex} />
+      </div>
     </div>
   );
 }
 
-function TabContent(props) {
-  if (props.index == 0) {
-    return <div>내용0</div>;
-  } else if (props.index == 1) {
-    return <div>내용1</div>;
-  } else if (props.index == 2) {
-    return <div>내용2</div>;
-  }
-  return null;
+function TabContent({ index }) {
+  let [fade, setFade] = useState("");
+
+  useEffect(() => {
+    setFade("");
+    setTimeout(() => {
+      setFade("fade_end");
+    }, 100);
+  }, [index]);
+
+  return (
+    <div className={`fade_start ${fade}`}>
+      {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][index]}
+    </div>
+  );
 }
 
 export default Detail;
